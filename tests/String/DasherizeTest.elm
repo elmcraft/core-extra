0511310386699ebb0dcbdf8300c1396df93f2103
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-09-04 13:04:39 +0200

    Fixed: Dasherize tests
    Breaks dasherize tests out into two separate ones. One checks that there is no whitespace in the resulting string. The second checks that there are no consecutive dashes in the resulting string.
    
    This replaces a previous test that was failing on bad input.

--- tests/String/DasherizeTest.elm
+++ tests/String/DasherizeTest.elm
@@ -1,11 +1,11 @@
 module String.DasherizeTest exposing (dasherizeTest)
 
-import Char
+import Char.Extra
 import Expect
 import Fuzz exposing (..)
-import String exposing (replace)
-import String.Extra exposing (..)
-import String.TestData as TestData
+import Regex exposing (Regex)
+import String
+import String.Extra exposing (dasherize)
 import Test exposing (..)
 
 
@@ -17,21 +17,29 @@ dasherizeTest =
                 dasherize s
                     |> String.toLower
                     |> Expect.equal (dasherize s)
-        , fuzz string "It replaces spaces and underscores with a dash" <|
+        , fuzz string "It has no spaces in the resulting string" <|
             \s ->
                 let
-                    expected =
-                        String.toLower
-                            >> String.trim
-                            >> replace "  " " "
-                            >> replace " " "-"
-                            >> replace "\t" "-"
-                            >> replace "\n" "-"
-                            >> replace "_" "-"
-                            >> replace "--" "-"
-                            >> replace "--" "-"
+                    whiteSpaceChecker =
+                        List.any Char.Extra.isSpace
                 in
                 dasherize (String.toLower s)
-                    |> String.toLower
-                    |> Expect.equal (expected s)
+                    |> String.toList
+                    |> whiteSpaceChecker
+                    |> Expect.equal False
+        , fuzz string "It has no consecutive dashes in the resulting string" <|
+            \s ->
+                let
+                    consecutiveDashesChecker =
+                        Regex.contains consecutiveDashesRegex
+                in
+                dasherize (String.toLower s)
+                    |> consecutiveDashesChecker
+                    |> Expect.equal False
         ]
+
+
+consecutiveDashesRegex : Regex
+consecutiveDashesRegex =
+    Regex.fromString "\\-{2,}"
+        |> Maybe.withDefault Regex.never
