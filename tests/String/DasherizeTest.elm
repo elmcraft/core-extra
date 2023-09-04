d5ac1080e12fc18b7481550452024af39fdf53e6
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-09-04 12:16:59 +0200

    Fixed: Remove uppercase test
    This PR removes the uppercase text test.
    
    The dasherize function was previously modified to not put the leading dash in the case of a capital letter starting the string, so this test is no longer needed. Any other cases are covered by existing tests.

--- tests/String/DasherizeTest.elm
+++ tests/String/DasherizeTest.elm
@@ -34,23 +34,4 @@ dasherizeTest =
                 dasherize (String.toLower s)
                     |> String.toLower
                     |> Expect.equal (expected s)
-        , fuzz TestData.randomStrings "It puts dash before every single uppercase character" <|
-            \s ->
-                dasherize s
-                    |> Expect.equal (replaceUppercase s |> String.toLower)
         ]
-
-
-replaceUppercase : String -> String
-replaceUppercase string =
-    string
-        |> String.toList
-        |> List.map
-            (\c ->
-                if Char.isUpper c then
-                    "-" ++ String.fromChar c
-
-                else
-                    String.fromChar c
-            )
-        |> String.concat
